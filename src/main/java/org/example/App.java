f5860238fca4f5d58f3702c36c5cb7f14679142c
tharindu-bandara <tharindub@wso2.com>
2020-01-11 02:58:08 +0530

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+/**
+ * Hello world!
+ */
+public class App {
+
+    public static void main(String[] args) {
+
+        try {
+            InputStream inputStream = new FileInputStream(args[1]);
+            throw new Exception();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("Hello World!");
+    }
+}
